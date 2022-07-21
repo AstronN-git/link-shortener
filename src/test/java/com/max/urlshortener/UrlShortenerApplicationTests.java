2c3a7b4e7a12fae86b922b15c75ac5aab2c88cfe
AstronN-git <max.sherb2007@gmail.com>
2022-07-21 22:04:10 +0300

    initial commit

---
+++ src/test/java/com/max/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
